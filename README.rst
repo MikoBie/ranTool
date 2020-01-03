c1409e56a53a14189579355a00c5508c511a9d9d
MikoBie <profesor_floretu@op.pl>
2020-01-03 19:59:30 +0100

    The app works but it needs documentation

---
+++ README.rst
@@ -0,0 +1,17 @@
+===============================================================================
+`RanTool` : A `PsychoPy` script to measure the ability to produce random series
+==============================================================================
+
+This is a `python` based script to conduct simple experiments on human-generated randomness. It allows to gather basic demographic data like: sex, age, faculty and dominant 
+hand. First it displays two intructions and afterward a red square which dictates the tempo of random series generation. It allows to only gather reposnses from two keys 
+('perion' and 'slash') but an easy modification might change it. It writes all the data to a `csv` format file. During the process of generating randomness the scrip allows to
+display the history of already produced elements. Therefore, it allows to run experiments in which in one condition participants last generated elements and in the second 
+condition they do not see them.
+
+The main script `ranTool.py` reads infomration about the condition from `conditions.csv` file which might be created with the use of `make_conditions.py` python script. By 
+defualt `make_conditions.py` created a random list of 50 visible history and 50 invisible history condition.
+
+=================
+Main Dependencies
+=================
+
--- ranTool.py
+++ ranTool.py
@@ -6,10 +6,19 @@ import re
 import glob
 import pandas
 
+## How many times the red square should display
+REPETITION_NUMBER = 5
+
+## How long the red square should be displayed in seconds
+RED_SQUARE_DISPLAY_LENGTH = .75
+
+## Time between red square's exposition in seconds
+RED_SQUARE_REST_LENGTH = 1.25
+
 ## read file with conditions
 conditions = pandas.read_csv('conditions.csv', sep = ';')
 
-## Create a pop up which gathers demogrphic data
+## Create a pop up which gathers demographic data
 myDlg = gui.Dlg(labelButtonOK='Kontynuuj', labelButtonCancel='Przerwij', size=[800,600])
 myDlg.addField(label='Id')
 myDlg.addField(label='Płeć', choices=['Kobieta','Mężczyzna'])
@@ -46,58 +55,76 @@ condition = conditions.loc[conditions['id']==int(myDlg.data[0])]['condition'].va
 if not myDlg.OK:
     sys.exit(0)
 
-## Creates a window
-#win = visual.Window(size = [800,600])
-## Creates a full screen window
+## Creates a full screen window on MacBook Pro Early 2015 13 inch
 win = visual.Window(fullscr=True, size=[1280,800])
-# Creates the stimuli (the red square)
-instruction = visual.TextStim(win, pos=(0,0))
-stim = visual.Rect(win, size = [200,200], lineColor = 'red', fillColor = 'red', units = 'pix')
-hist0 = visual.TextStim(win, text='', pos=(1,.8))
-hist1 = visual.TextStim(win, text='', pos=(.9,.8))
-hist2 = visual.TextStim(win, text='', pos=(.8,.8))
-hist3 = visual.TextStim(win, text='', pos=(.7,.8))
-hist4 = visual.TextStim(win, text='', pos=(.6,.8))
-hist5 = visual.TextStim(win, text='', pos=(.5,.8))
-hist6 = visual.TextStim(win, text='', pos=(.4,.8))
+
+# Creates the instruction objects sex specific
+if myDlg.data[1] == 'Kobieta':
+    instruction = visual.ImageStim(win,image='instrukcja_k/instrukcja.png', size=(1.92,1.2))
+    instruction2 = visual.ImageStim(win,image='instrukcja2_k/instrukcja2.png', size=(1.92,1.3969904240766073))
+else:
+    instruction = visual.ImageStim(win,image='instrukcja_m/instrukcja.png', size=(1.92,1.2))
+    instruction2 = visual.ImageStim(win,image='instrukcja2_m/instrukcja2.png', size=(1.92,1.3969904240766073))
+
+## Creates red square object
+stim = visual.Rect(win, size = [800,800], lineColor = 'red', fillColor = 'red', units = 'pix')
+
+## Creates objects for displaying history
+hist0 = visual.TextStim(win, text='', pos=(1,.8), font='Latin Modern Roman')
+hist1 = visual.TextStim(win, text='', pos=(.9,.8), font='Latin Modern Roman')
+hist2 = visual.TextStim(win, text='', pos=(.8,.8), font='Latin Modern Roman')
+hist3 = visual.TextStim(win, text='', pos=(.7,.8), font='Latin Modern Roman')
+hist4 = visual.TextStim(win, text='', pos=(.6,.8), font='Latin Modern Roman')
+hist5 = visual.TextStim(win, text='', pos=(.5,.8), font='Latin Modern Roman')
+hist6 = visual.TextStim(win, text='', pos=(.4,.8), font='Latin Modern Roman')
 hist = [hist6,hist5,hist4,hist3,hist2,hist1,hist0]
-## Opens the otuput file
 
+## Creates ending object
+end = visual.TextStim(win, text='Koniec tej części badania.', pos=(0.5,0), font='Latin Modern Roman')
 
-## Initialize the clock and 
+## Initialize the clock and clear keys buffer
 myClock=clock.Clock()
-
 event.clearEvents()
-with open('Instrukcja.txt', 'r') as file:
-    instruction.text = file.read()
+
+
+## Display first Instruction
 instruction.draw()
 win.flip()
 event.waitKeys(keyList = ['space'], clearEvents=True)
+
+## Display second instruction
+instruction2.draw()
+win.flip()
+event.waitKeys(keyList = ['space'], clearEvents=True)
+
+## Creates file name
 file_name = 'data/' + condition + '_' + myDlg.data[0] + '.csv'
 
+## Opens a file connection
 with open(file_name,'w') as file:
     file.write('id;sex;hand;age;faculty;condition;trial;time;key'+'\n')
-    ## Starts the routine of displaying the square 
+    
+    ## Starts the routine of displaying the red square 
     continueRutine=True
-
-    for trial in range(5):
+    for trial in range(REPETITION_NUMBER):
         myClock.reset()
         event.clearEvents()
         myKeys=[]
         i = 0
 
         while continueRutine:
-            if myClock.getTime() <= .750:
+            if myClock.getTime() <= RED_SQUARE_DISPLAY_LENGTH:
                 myKeys.append(event.getKeys(keyList=['period','slash'], timeStamped=myClock))
                 stim.autoDraw=True
                 win.flip()
-            if myClock.getTime() > .750:
+            if myClock.getTime() > RED_SQUARE_DISPLAY_LENGTH:
                 myKeys.append(event.getKeys(keyList=['period','slash'], timeStamped=myClock))
                 stim.autoDraw=False
                 win.flip()
-            if myClock.getTime() > 2:
+            if myClock.getTime() > (RED_SQUARE_DISPLAY_LENGTH + RED_SQUARE_REST_LENGTH):
                 continueRutine=False
             
+            ## Display history if the condition is visible
             if len(myKeys[i])>0 and condition=='visible':
                 if myKeys[i][0][0] == 'slash':
                     text = 'R'
@@ -116,6 +143,7 @@ with open(file_name,'w') as file:
                     h.autoDraw = True
             i = i + 1
 
+        ## Write out every single button pressed to the file
         for line in myKeys:
             if len(line) > 0:
                 output = [myDlg.data[0],myDlg.data[1],myDlg.data[2],myDlg.data[3],myDlg.data[4],condition, trial, line[0][1],line[0][0]]
@@ -123,3 +151,11 @@ with open(file_name,'w') as file:
         
 
         continueRutine=True
+
+## Display ending information
+for h in hist:
+    h.autoDraw = False
+win.flip()
+end.draw()
+win.flip()
+event.waitKeys(keyList = ['backslash'], clearEvents=True)
